7eaba1cf57a083eb7a4fdd5a93dd382ffc858c1d
Slepovron <vladimir.slepovron@gmail.com>
2020-04-04 01:38:24 +0300

    lesson - webpack-app upd

--- lessons/webpack-app/src/app.js
+++ lessons/webpack-app/src/app.js
@@ -1,3 +1,30 @@
+import {
+  isValid
+} from "../utils";
 import "./styles.css";
 
-console.log("App in progress...");
+
+// console.log("App in progress..."); // сообщение-затычка
+
+const form = document.querySelector('#form');
+const input = form.querySelector('#form-input');
+const submitBtn = form.querySelector('#form-submit');
+
+form.addEventListener('submit', submitFormHandler); // submit это нативное событие подтверждения
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  //console.log(input.value); //сообщение заглушка
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(), //trim удалит лишние пробелы
+      date: new Date().toJSON() // добавит дату в JSON  формате
+    };
+
+    submitBtn.disabled = true;
+    console.log('Pytanie', question);
+    // async request to server for save question
+  }
+}
