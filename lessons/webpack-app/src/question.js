463ab8795d580dbda9dbf00f9a62e7a4c8162b9a
Slepovron <vladimir.slepovron@gmail.com>
2020-05-09 01:52:01 +0300

    web security lessons

--- lessons/webpack-app/src/question.js
+++ lessons/webpack-app/src/question.js
@@ -1,14 +1,14 @@
 export class Question {
   static create(question) {
-    return fetch('https://slepovron-site-app.firebaseio.com/questions.json', {
-        method: 'POST',
-        body: JSON.stringify(question),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(response => {
+    return fetch("https://slepovron-site-app.firebaseio.com/questions.json", {
+      method: "POST",
+      body: JSON.stringify(question),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
         console.log(response);
       });
     // .then(addToLocalStorage)
