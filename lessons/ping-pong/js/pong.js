f74eff6830ee6d1ae1de81d39d540071c34fccd3
Slepovron <vladimir.slepovron@gmail.com>
2020-06-14 20:01:14 +0300

    update ping-pong

--- lessons/ping-pong/js/pong.js
+++ lessons/ping-pong/js/pong.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('pong');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("pong");
+const ctx = canvas.getContext("2d");
 
 // поле задается через html и стили
 
@@ -11,8 +11,8 @@ const ball = {
   velocityX: 5, // векторная скорость по Х
   velocityY: 5, // векторная скорость по У
   speed: 7, // скорость
-  color: "WHITE"
-}
+  color: "WHITE",
+};
 
 // сеть для поля (можно задать дополнительные свойства)
 const net = {
@@ -20,8 +20,8 @@ const net = {
   y: 0, // рисуем сеть от верхней границы поля
   height: 10, // высота полоски сети
   width: 2, // толщина
-  color: "WHITE"
-}
+  color: "WHITE",
+};
 
 // игрок 1
 const user1 = {
@@ -30,8 +30,8 @@ const user1 = {
   width: 10,
   height: 100,
   score: 0,
-  color: "WHITE"
-}
+  color: "WHITE",
+};
 
 // игрок 2 (или компьютер)
 const user2 = {
@@ -40,8 +40,8 @@ const user2 = {
   width: 10,
   height: 100,
   score: 0,
-  color: "WHITE"
-}
+  color: "WHITE",
+};
 
 // квадратные объекты
 function drawRect(x, y, w, h, color) {
@@ -68,7 +68,7 @@ function drawNet() {
 // текст (очки)
 function drawText(text, x, y) {
   ctx.fillStyle = "#FFF";
-  ctx.font = "75px fantasy";
+  ctx.font = "75px arial";
   ctx.fillText(text, x, y);
 }
 
@@ -81,45 +81,16 @@ function resetBall() {
 }
 
 //управление user1
-// canvas.addEventListener("mousemove", getMousePos);
+document.addEventListener("mousemove", getMousePos);
 
-// function getMousePos(evt) {
-//   let rect = canvas.getBoundingClientRect();
-
-//   user1.y = evt.clientY - rect.top - user1.height / 2;
-// }
-
-document.addEventListener('keypress', getKeyPress);
-
-function getKeyPress(event) {
-  let key = event.keyCode;
+function getMousePos(evt) {
   let rect = canvas.getBoundingClientRect();
-  console.log(key);
-  if (key == 115) { // DOWN
-    user1.y = event.clientY - rect.top - user1.height / 2;
 
-  } else if (key == 119) { // UP
-    user1.y = Math.max(0, user1.y - 8);
-  }
-
-  if (key == 40) { // DOWN
-    user2.y = Math.min(canvas.height - user2.height, user2.y + 4);
-  } else if (key == 38) { // UP
-    user2.y = Math.max(0, user2.y - 4);
-  }
+  user1.y = evt.clientY - rect.top - user1.height / 2;
 }
 
-//-----
 
-// when COM or USER scores, we reset the ball
-function resetBall() {
-  ball.x = canvas.width / 2;
-  ball.y = canvas.height / 2;
-  ball.velocityX = -ball.velocityX;
-  ball.speed = 7;
-}
-
-// collision detection
+// события игрока и мяча
 function collision(b, p) {
   p.top = p.y;
   p.bottom = p.y + p.height;
@@ -131,13 +102,15 @@ function collision(b, p) {
   b.left = b.x - b.radius;
   b.right = b.x + b.radius;
 
-  return p.left < b.right && p.top < b.bottom && p.right > b.left && p.bottom > b.top;
+  return (
+    p.left < b.right && p.top < b.bottom && p.right > b.left && p.bottom > b.top
+  );
 }
 
-// update function, the function that does all calculations
+//-----
+// рассчет перемещений и событий (скопипащщен без изменений)
 function update() {
-
-  // change the score of players, if the ball goes to the left "ball.x<0" computer win, else if "ball.x > canvas.width" the user win
+  // подсчет очков при пересечении мячом границ поля по X
   if (ball.x - ball.radius < 0) {
     user2.score++;
     resetBall();
@@ -146,26 +119,25 @@ function update() {
     resetBall();
   }
 
-  // the ball has a velocity
+  // векторная скорость мяча
   ball.x += ball.velocityX;
   ball.y += ball.velocityY;
 
-  // computer plays for itself, and we must be able to beat it
-  // simple AI
-  // user2.y += ((ball.y - (user2.y + user2.height / 2))) * 0.1;
+  // второй игрок управляется компьютером (отслеживает позицию мяча по y с внесением искусственного отставания)
+  user2.y += ((ball.y - (user2.y + user2.height / 2))) * 0.05;
 
-  // when the ball collides with bottom and top walls we inverse the y velocity.
+  // инверсия вектора мяча при соприкосновении с верхней и нижней границей поля
   if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
     ball.velocityY = -ball.velocityY;
   }
 
-  // we check if the paddle hit the user or the com paddle
-  let player = (ball.x + ball.radius < canvas.width / 2) ? user1 : user2;
+  // проверка соприкосновения мяча с полями игроков
+  let player = ball.x + ball.radius < canvas.width / 2 ? user1 : user2;
 
-  // if the ball hits a paddle
+  // мяч попал по полю игрока
   if (collision(ball, player)) {
-    // we check where the ball hits the paddle
-    let collidePoint = (ball.y - (player.y + player.height / 2));
+    // проверяем в какой точке произошло попадание
+    let collidePoint = ball.y - (player.y + player.height / 2);
     // normalize the value of collidePoint, we need to get numbers between -1 and 1.
     // -player.height/2 < collide Point < player.height/2
     collidePoint = collidePoint / (player.height / 2);
@@ -176,27 +148,25 @@ function update() {
     // Math.PI/4 = 45degrees
     let angleRad = (Math.PI / 4) * collidePoint;
 
-    // change the X and Y velocity direction
-    let direction = (ball.x + ball.radius < canvas.width / 2) ? 1 : -1;
+    // изменение векторов движения мяча
+    let direction = ball.x + ball.radius < canvas.width / 2 ? 1 : -1;
     ball.velocityX = direction * ball.speed * Math.cos(angleRad);
     ball.velocityY = ball.speed * Math.sin(angleRad);
 
-    // speed up the ball everytime a paddle hits it.
+    // увеличиваем скорость мяча при каждом удачном отбитии
     ball.speed += 0.1;
   }
 }
-
 //-----
 
 // отрисовка
 function render() {
-
   // очищаем все поле на каждом кадре отрисовки
   drawRect(0, 0, canvas.width, canvas.height, "#343434");
 
   // записываем очки
   drawText(user1.score, canvas.width / 4, canvas.height / 5);
-  drawText(user2.score, 3 * canvas.width / 4, canvas.height / 5);
+  drawText(user2.score, (3 * canvas.width) / 4, canvas.height / 5);
 
   // сеть
   drawNet();
@@ -209,13 +179,12 @@ function render() {
   drawArc(ball.x, ball.y, ball.radius, ball.color);
 }
 
-
 function game() {
   update();
   render();
 }
-// number of frames per second
+// количество фреймов в секунду
 let framePerSecond = 50;
 
-//call the game function 50 times every 1 Sec
+// вызываем функцию game 50 раз в секунду
 let loop = setInterval(game, 1000 / framePerSecond);
